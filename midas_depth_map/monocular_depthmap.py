2128289ace21b5d909b01a65fe3c5ec66c3640b0
hamedzeinalzadeh <hamedzeinal@yahoo.com>
2023-12-26 01:34:56 -0800

    depthmap script updated, pytorch instead of cv.dnn

--- midas_depth_map/monocular_depthmap.py
+++ midas_depth_map/monocular_depthmap.py
@@ -1,80 +1,65 @@
+import cv2
+import torch
+import numpy as np
 import time
 
-import cv2
-import numpy as np
+# Load MiDaS model from torch.hub
+model_type = "MiDaS_small"  # Choose from DPT_Large, DPT_Hybrid, or MiDaS_small
+model = torch.hub.load("intel-isl/MiDaS", model_type)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+model.eval()
 
-path_model = "models/"
+# Load MiDaS transforms
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+    transform = midas_transforms.dpt_transform
+else:
+    transform = midas_transforms.small_transform
 
-# Read Network
-model_name = "model-f6b98070.onnx"  # MiDaS v2.1 Large
-# model_name = "model-small.onnx"; # MiDaS v2.1 Small
-
-
-# Load the DNN model
-model = cv2.dnn.readNet(path_model + model_name)
-
-
-if (model.empty()):
-    print("Could not load the neural net! - Check path")
-
-
-# Set backend and target to CUDA to use GPU
-model.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-model.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-
-# Webcam
+# Initialize video capture
 cap = cv2.VideoCapture(0)
 
-
 while cap.isOpened():
+    start_time = time.time()
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-    # Read in the image
-    success, img = cap.read()
+    # Preprocess the image
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    input_batch = transform(img).to(device)
 
-    imgHeight, imgWidth, channels = img.shape
+    # Model inference
+    with torch.no_grad():
+        prediction = model(input_batch)
 
-    # start time to calculate FPS
-    start = time.time()
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=(frame.shape[0], frame.shape[1]),
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    # Create Blob from Input Image
-    # MiDaS v2.1 Large ( Scale : 1 / 255, Size : 384 x 384, Mean Subtraction : ( 123.675, 116.28, 103.53 ), Channels Order : RGB )
-    blob = cv2.dnn.blobFromImage(
-        img, 1/255., (384, 384), (123.675, 116.28, 103.53), True, False)
-
-    # MiDaS v2.1 Small ( Scale : 1 / 255, Size : 256 x 256, Mean Subtraction : ( 123.675, 116.28, 103.53 ), Channels Order : RGB )
-    # blob = cv2.dnn.blobFromImage(img, 1/255., (256,256), (123.675, 116.28, 103.53), True, False)
-
-    # Set input to the model
-    model.setInput(blob)
-
-    # Make forward pass in model
-    output = model.forward()
-
-    output = output[0, :, :]
-    output = cv2.resize(output, (imgWidth, imgHeight))
+    output = prediction.cpu().numpy()
 
     # Normalize the output
     output = cv2.normalize(
         output, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
-    # End time
-    end = time.time()
-    # calculate the FPS for current frame detection
-    fps = 1 / (end-start)
-    # Show FPS
-    cv2.putText(img, f"{fps:.2f} FPS", (20, 30),
+    # Calculate FPS
+    fps = 1.0 / (time.time() - start_time)
+    cv2.putText(frame, f"FPS: {fps:.2f}", (20, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-    cv2.imshow('image', img)
+    # Show the original frame and depth map
+    cv2.imshow('Original', frame)
     cv2.imshow('Depth Map', output)
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
+    # Break loop with 'q' key
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-
+# Release the video capture object and close windows
 cap.release()
 cv2.destroyAllWindows()
